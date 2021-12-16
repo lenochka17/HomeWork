ce78d97a269dd6142ac3b357bd5d8bf5b02f5d9d
lenochka17 <l.batanova17@gmail.com>
2021-12-16 15:49:48 +0300

    Add methods

---
+++ src/com/javacourse/se/lesson2/Main.java
@@ -0,0 +1,35 @@
+package com.javacourse.se.lesson2;
+
+public class Main {
+    public static void main(String[]args) {
+        int a = 5;
+        int b = 4;
+        int sum = a + b;
+
+        boolean flag = within10and20(sum);
+
+        System.out.println(flag);
+
+        //2
+        int number =10;
+        printNumber(number);
+        //3
+        int c = 4;
+        boolean check = noOrYes(c);
+        System.out.println(check);
+    }
+
+    static boolean within10and20(int sum) {
+        return sum >= 10 && sum <= 20;
+    }
+    static void printNumber(int number) {
+        if (0 <= number) {
+            System.out.println("+");
+        } else {
+            System.out.println("-");
+        }
+    }
+    static boolean noOrYes(int c) {
+        return c > 0;
+    }
+}
