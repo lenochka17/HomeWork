9f13381966a285458c0a4032000fb9c6fe74f8cd
lenochka17 <l.batanova17@gmail.com>
2021-12-16 16:38:12 +0300

    Add method PrintWordNTimes

--- src/com/javacourse/se/lesson2/Main.java
+++ src/com/javacourse/se/lesson2/Main.java
@@ -1,27 +1,33 @@
 package com.javacourse.se.lesson2;
 
 public class Main {
-    public static void main(String[]args) {
+    public static void main(String[] args) {
         int a = 5;
         int b = 4;
         int sum = a + b;
 
         boolean flag = within10and20(sum);
-
         System.out.println(flag);
 
         //2
-        int number =10;
+        int number = 10;
         printNumber(number);
+
         //3
         int c = 4;
         boolean check = noOrYes(c);
         System.out.println(check);
+
+        //4
+        printWordNTimes();
+
+
     }
 
     static boolean within10and20(int sum) {
         return sum >= 10 && sum <= 20;
     }
+
     static void printNumber(int number) {
         if (0 <= number) {
             System.out.println("+");
@@ -29,7 +35,18 @@ public class Main {
             System.out.println("-");
         }
     }
+
     static boolean noOrYes(int c) {
         return c > 0;
+    }
+
+    static void printWordNTimes() {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 1; j++) {
+                System.out.print("String");
+            }
+            System.out.println();
+        }
+
     }
 }
