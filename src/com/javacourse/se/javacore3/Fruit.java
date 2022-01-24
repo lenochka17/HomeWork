adfc0d86593525ce61311091d932df6b03d48313
lenochka17 <l.batanova17@gmail.com>
2022-01-24 16:45:48 +0300

    Added HomeWork3

---
+++ src/com/javacourse/se/javacore3/Fruit.java
@@ -0,0 +1,8 @@
+package com.javacourse.se.javacore3;
+
+public abstract class Fruit {
+    float weight;
+
+    public abstract float getWeight();
+
+}
---
+++ src/com/javacourse/se/javacore3/Orange.java
@@ -0,0 +1,10 @@
+package com.javacourse.se.javacore3;
+
+public class Orange extends Fruit{
+
+    float weight = 1.5f;
+
+    public float getWeight() {
+        return weight;
+    }
+}
